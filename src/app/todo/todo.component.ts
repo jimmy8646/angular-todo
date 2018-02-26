20134cb6d50a86486534ca1c778bd60140749297
jimmy8646 <yujiechueh@gmail.com>
2018-02-26 20:39:00 +0800

    push work

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from './shared/todo.service';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
+  providers: [TodoService],
 })
 export class TodoComponent implements OnInit {
-
-  constructor() { }
+  toDoListArray: any[];
+  constructor(private toDoService: TodoService) { }
 
   ngOnInit() {
+    this.toDoService.getToDoList().snapshotChanges()
+      .subscribe(item => {
+        this.toDoListArray = [];
+        item.forEach(element => {
+          var x = element.payload.toJSON();
+          x["$key"] = element.key;
+          this.toDoListArray.push(x);
+        })
+
+        //未完成先排
+        this.toDoListArray.sort((a, b) => {
+          return a.isChecked - b.isChecked;
+        })
+
+      });
   }
+
+  onAdd(itemTitle) {
+   this.toDoService.addTitle(itemTitle.value);
+   itemTitle.value = null;
+ }
 
 }
